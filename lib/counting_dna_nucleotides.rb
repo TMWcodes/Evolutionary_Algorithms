efc9cfba5c071442a819ac7343d38f39d83f0e64
Tyrone Williams <Tyronemwilliams@outlook.com>
2022-09-28 17:09:44 +0100

    feat: count multiple nucleotides

--- lib/counting_dna_nucleotides.rb
+++ lib/counting_dna_nucleotides.rb
@@ -7,8 +7,19 @@ def get_counted_nucleotides(dna)
  nucleobases.each_char do |char| 
     counted[char] = 0
  end
+
+
+
+ dna.each_char do |base|
+    case base
+    when "T"
+     counted['T'] += 1   
+    end
+    
+ end
+    
  counted
  
 end
 
-get_counted_nucleotides('')
+get_counted_nucleotides('TTT')
--- spec/counting_dna_nucleotides_spec.rb
+++ spec/counting_dna_nucleotides_spec.rb
@@ -1,9 +1,17 @@
 require 'counting_dna_nucleotides.rb'
 
 describe 'get_counted_nucleotides' do
+
     context 'when given no input' do
         it 'returns 0 for each nucleobase' do
             expect(get_counted_nucleotides('')).to eq({"A" => 0, "C" => 0, "G" => 0, "T" => 0})
         end
     end
+
+    context 'when given less than 4 nucleobases' do
+        it 'counts nucleotides' do
+        expect(get_counted_nucleotides('TTT')).to eq({"A" => 0, "C" => 0, "G" => 0, "T" => 3});
+        end
+    end
+
 end
