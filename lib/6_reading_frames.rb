d56b5584cd376fb975a79f0c09b2c297a6b4faaa
Tyrone Williams <Tyronemwilliams@outlook.com>
2022-11-04 16:08:33 +0000

    FEAT: add ability to read in 6 reading frames

---
+++ lib/6_reading_frames.rb
@@ -0,0 +1,20 @@
+def decompose_double_strand(seq1, seq2)
+    output = ""
+    
+    frame1 = seq1.scan(/.../).join" "
+    f2 = seq1[1..-3].scan(/.../).join" "
+    frame2 = seq1[0] + " " + f2 + " " + seq1[-2..-1]
+    f3 = seq1[2..-2].scan(/.../).join" "
+    frame3 = seq1[0..1] + " " + f3 + " " + seq1[-1]
+    output << ["Frame 1: #{frame1}", "Frame 2: #{frame2}", "Frame 3: #{frame3}"].join("\n")
+
+    frame1 = seq2.scan(/.../).join" "
+    f2 = seq2[1..-3].scan(/.../).join" "
+    frame2 = seq2[0] + " " + f2 + " " + seq2[-2..-1]
+    f3 = seq2[2..-2].scan(/.../).join" "
+    frame3 = seq2[0..1] + " " + f3 + " " + seq2[-1]
+    output << ["\nReverse Frame 1: #{frame1}", "Reverse Frame 2: #{frame2}", "Reverse Frame 3: #{frame3}"].join("\n")
+    output
+end
+
+# puts decompose_double_strand("AGGTGACACCGCAAGCCTTATATTAGC", "GCTAATATAAGGCTTGCGGTGTCACCT")
--- spec/gene_integration_spec.rb
+++ spec/gene_integration_spec.rb
@@ -3,6 +3,8 @@ require 'base_pairs'
 require 'dna_sequence_tester'
 require 'dna_to_rna'
 require 'rna_to_protein_sequence'
+require '3_reading_frame'
+require '6_reading_frames'
 
 describe 'transcription' do
     context 'after creating a dna strand' do
@@ -17,19 +19,26 @@ describe 'transcription' do
         end
     end
 
-    context 'after creating a dna strand' do
-        it 'makes dna into rna' do
+    context 'after creating a double helix' do
+        it 'makes dna into mrna' do
             seq1 = "atgacatgtt".upcase
             seq2 = DNA_strand(seq1)
            expect(dna_to_rna(seq1)).to eq('AUGACAUGUU')
            expect(dna_to_rna(seq2)).to eq('UACUGUACAA')
         end
 
-        it 'turns rna into aminoacids' do
+        it 'turns mrna into aminoacids' do
             seq1 = "atgacatgtt".upcase
             seq2 = DNA_strand(seq1)
             mRNA = dna_to_rna(seq1) 
             expect(protein(mRNA)).to eq('MTC')
+        end
+        it 'creates 6 reading frames' do
+            seq1 = "AGGTGACACCGCAAGCCTTATATTAGC"
+            seq2 = DNA_strand(seq1).reverse #complementary_dna.
+            check_DNA(seq1, seq2)
+            expect(decompose_single_strand(seq2)).to eq("Frame 1: GCT AAT ATA AGG CTT GCG GTG TCA CCT, Frame 2: G CTA ATA TAA GGC TTG CGG TGT CAC CT, Frame 3: GC TAA TAT AAG GCT TGC GGT GTC ACC T")
+            expect(decompose_double_strand(seq1, seq2)).to eq("Frame 1: AGG TGA CAC CGC AAG CCT TAT ATT AGC\nFrame 2: A GGT GAC ACC GCA AGC CTT ATA TTA GC\nFrame 3: AG GTG ACA CCG CAA GCC TTA TAT TAG C\nReverse Frame 1: GCT AAT ATA AGG CTT GCG GTG TCA CCT\nReverse Frame 2: G CTA ATA TAA GGC TTG CGG TGT CAC CT\nReverse Frame 3: GC TAA TAT AAG GCT TGC GGT GTC ACC T")
         end
     end
 
